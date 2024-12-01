7dac21aaaef6ef3ca723dc4456347dc36006289d
IsraelKariti <89970476+IsraelKariti@users.noreply.github.com>
2024-12-01 22:50:39 +0200

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const HomePage = ()=>{
     return <>
-        <div>home page! crazy!bitch!finaly!</div>
+        <div>home page! crazy!bitch!finaly!clearrrrr</div>
         <Link to="/profiles">to profiles</Link>
     </> 
 }
