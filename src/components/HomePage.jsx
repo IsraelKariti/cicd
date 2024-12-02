feab4a42028dc506a93cc11b783b98cc0475653a
IsraelKariti <89970476+IsraelKariti@users.noreply.github.com>
2024-12-02 10:02:29 +0200

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const HomePage = ()=>{
     return <>
-        <div>home page! crazy!bitch!finaly!clearrrrr acl all the way fresh start east sad bucket name</div>
+        <div>home page! crazy!bitch!finaly!clearrrrr acl all the way fresh start east sad bucket name with ACLs</div>
         <Link to="/profiles">to profiles</Link>
     </> 
 }
